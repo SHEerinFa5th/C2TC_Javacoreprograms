7a5a70f9636c4e95f8e7d5a2469dcbe97f7ebfb3
rekal <rekal@Rekz>
2025-08-14 16:22:15 +0530

    i have uploaded

---
+++ corejavaprograms1/src/day2/Example.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Example {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+	}
+
+}
